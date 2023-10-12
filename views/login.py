4ca9b58002957685632021aed56d7f41a559bc2e
BYEONGRYEOL <lyulbyung@gmail.com>
2023-10-12 10:04:14 +0900

    pull 한 blueprint로 작업중인 myaccount 에서 merge 완료

--- views/login.py
+++ views/login.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request, url_for, session, redirect, flash
+import datetime
+from flask import Blueprint, jsonify, render_template, request, url_for, session, redirect, flash
+from jwt import encode
+import jwt
+from config import JWT_SECRET_KEY
 from models.user import User
 from db_connect import db
 
@@ -9,15 +13,27 @@ bp = Blueprint('login', __name__, url_prefix='/login')
 @bp.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
-        if validate_login(username, password):
-            # jwt 토큰?
+        user = User.query.filter_by(email=email).first()
+        if user and validate_login(email, password):  # 로그인이 유효한 경우
 
-            return redirect(url_for('/storelist')) # TODO: mainpage.함수명 수정
+            # TypeError: Object of type datetime is not JSON serializable 해결하기 위해 string으로 변환
+            dateString = (datetime.datetime.now()
+                          + datetime.timedelta(seconds=60*60*1)).strftime("%Y-%m-%d %H:%M:%S")
+
+            payload = {'email': email, 'expire': dateString}
+            print(payload)
+
+            token = encode(payload, JWT_SECRET_KEY,
+                           algorithm='HS256')  # 응답 jsonify 객체 생성
+
+            return jsonify({'result': 'success', 'token': token})
+            # jwt 토큰?
         else:
-            error = '비밀번호가 틀렸습니다.'
-            return render_template('login.html', error=error)
+            flash('비밀번호가 틀렸습니다', 'error')
+            return render_template('login.html')
+
     return render_template('login.html')
 
 
@@ -26,3 +42,17 @@ def validate_login(email, password):
     if user:
         return True
     return False
+
+
+@bp.route('/token-check')
+def checkTokenVaild():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, JWT_SECRET_KEY,
+                             algorithms=['HS256'])
+        print(payload)
+        return render_template('storelist.html')
+    except jwt.ExpiredSignatureError:
+        return render_template('login.html')
+    except:
+        return render_template('login.html')
